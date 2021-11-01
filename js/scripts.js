86c4553031199e354831b5c6aecb8456aa0694bb
gestevesf <92437360+gestevesf@users.noreply.github.com>
2021-11-01 17:05:58 +0100

    updated scripts.js

--- js/scripts.js
+++ js/scripts.js
@@ -1,4 +1,20 @@
-<script>
+console.log ("Hello! Welcome to my Portfolio Site. In here you can see what I've been working on and who am I. If you have any question, please reach out to me through any of the contact buttons in the end of the page. Thank you.");
+
+var age = 29;
+var name = 'Gonçalo';
+console.log (name);
+console.log (age);
+
+
+
+
+
+
+
+
+
+
+
 var slideIndex = [1,1];
 var slideId = ["mySlides1", "mySlides2"]
 showSlides(1, 0);
@@ -18,4 +34,3 @@ function showSlides(n, no) {
   }
   x[slideIndex[no]-1].style.display = "block";
 }
-</script>
